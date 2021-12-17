ee03a11cad52bdee57c060ec29822436c1d0f6ce
lyl <liyilongaa@163.com>
2021-12-17 13:09:36 +0800

    两种路由模式

---
+++ 42_src_history和hash两种路由模式/pages/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>我是About内容</h2>
+</template>
+
+<script>
+export default {
+  name: "About",
+  beforeDestroy() {
+    console.log('About组件即将被销毁了')
+  },
+  mounted() {
+    console.log('About组件挂载了')
+  },
+  // 通过路由规则，进入该组件被调用
+  beforeRouteEnter(to, from, next) {
+    console.log('About---beforeRouteEnter', to, from)
+    if(to.meta.isAuth) {
+        if(localStorage.getItem('school') === 'atguigu') {
+            next()
+        } else {
+            alert('学校名不对，无权限查看！')
+        }
+    } else {
+        next()
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('About---beforeRouteleave')
+    next()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
