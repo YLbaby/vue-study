59b8a9c4b21ee864bae4a525e6d86fd044753c16
lyl <liyilongaa@163.com>
2021-12-17 12:39:44 +0800

    组件内路由守卫

---
+++ 41_src_组件内路由守卫/pages/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Home组件内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link
+              replace
+              class="list-group-item"
+              active-class="active"
+              to="/home/news">
+            Newsaaa
+          </router-link>
+        </li>
+        <li>
+          <router-link
+              replace
+              class="list-group-item"
+              active-class="active"
+              to="/home/message"
+          >
+            Message
+          </router-link>
+        </li>
+      </ul>
+      <!--include指的是组件名!-->
+      <keep-alive include="News">
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  beforeDestroy() {
+    console.log('Home组件即将被销毁了')
+  },
+  mounted() {
+    console.log('Home组件挂载完毕了')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
