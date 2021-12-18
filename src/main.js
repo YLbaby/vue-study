2c5d69558d5acd10ab907699d4e15a88706c0e33
lyl <liyilongaa@163.com>
2021-12-18 17:54:04 +0800

    vue2 complete

--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import Vue from 'vue'
 // 引入App
 import App from './App'
 
-// 引入VueRouter
-import VueRouter from 'vue-router'
-// 引入路由器
-import router from "./router";
+// 按需引入
+import {Button, Select, Row} from 'element-ui'
 
-// 应用插件
-Vue.use(VueRouter)
-
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+Vue.component(Row.name, Row)
 Vue.config.productionTip = false
-
-
+// elementUI
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// 应用elementUI
+// Vue.use(ElementUI)
 // 创建vm
 new Vue({
   el:'#app',
   render: h => h(App),
-  router
 })
